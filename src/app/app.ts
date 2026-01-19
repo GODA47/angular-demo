356f5a29c450526fca13cb5858d9aa74d18c941b
GODA47 <git@stackblitz.com>
2026-01-19 16:54:19 +0700

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styles: ``,
+  template: `
+    <div class="login">
+      <div>Demo Login Page</div>
+      
+    </div>
+  `,
+})
+export class App {}
